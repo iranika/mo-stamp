334cfa0a7cff8c7ec27d4d075233e3cd632f4baf
iranika <7370486+iranika@users.noreply.github.com>
2021-07-15 00:12:06 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -19,7 +19,11 @@ importScripts(
 
 workbox.core.setCacheNameDetails({prefix: "stamp"});
 
-workbox.core.skipWaiting();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
